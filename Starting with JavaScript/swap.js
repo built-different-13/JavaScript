435ef6c7583c3ca45b516194d396c2c230487713
built-different-13 <built.different7813@gmail.com>
2025-07-14 00:46:56 +0530

    Add files via upload

---
+++ Starting with JavaScript/swap.js
@@ -0,0 +1,17 @@
+var a = 5;
+var b = 10;
+
+console.log("Before Swapping");
+
+console.log("a is " + a);
+console.log("b is " + b);
+
+
+var temp = a;
+a = b;
+b = temp;
+
+console.log("After Swapping:");
+
+console.log("a is " + a);
+console.log("b is " + b);
