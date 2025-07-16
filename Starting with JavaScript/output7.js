3b6983b894a31289075a7f1aa58811eed061bbc4
built-different-13 <built.different7813@gmail.com>
2025-07-17 00:06:28 +0530

    Add files via upload

---
+++ Starting with JavaScript/output7.js
@@ -0,0 +1,12 @@
+var a = 10;
+console.log(typeof(a));
+a = "Dhara";
+console.log(typeof(a));
+a = true;
+console.log(typeof(a));
+a = null;
+console.log(typeof(a));
+a = Infinity;
+console.log(typeof(a));
+a = NaN;
+console.log(typeof(a));
