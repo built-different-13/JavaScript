aa02d06035eb9867d111ce5ebf39ef20f4c7f6ba
built-different-13 <built.different7813@gmail.com>
2025-07-19 15:34:24 +0530

    Add files via upload

---
+++ Starting with JavaScript/conditionals.js
@@ -0,0 +1,36 @@
+var a = 0;
+if(a>0) {
+    console.log("Positive");
+} else if(a<0) {
+    console.log("Negative");
+} else if(a==0) {
+    console.log("Zero");
+}
+
+var b = 0;
+if(b) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+var c = "";
+if(c) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+var d;
+if(d) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+var e = null;
+if(e) {
+    console.log("True");
+} else {
+    console.log("False");
+}
