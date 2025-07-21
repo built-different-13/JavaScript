5f275b1e10d6119036cd987aa0240b05d5d1218d
built-different-13 <built.different7813@gmail.com>
2025-07-21 10:29:18 +0530

    Add files via upload

---
+++ Starting with JavaScript/loops.js
@@ -0,0 +1,17 @@
+var i = 1;
+console.log("The numbers from " + i + " to 5 are printed below-:");
+while(i<=5) {
+    console.log(i);
+    i++;
+}
+
+var i=1;
+for (; i<=10; ++i) {
+    console.log(i);
+}
+
+var i=1;
+do{
+    console.log(i);
+    i++;
+} while(i<=5);
