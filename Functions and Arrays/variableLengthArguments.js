511a513a44fcd216bf37419467ecfd036ec56692
built-different-13 <built.different7813@gmail.com>
2025-07-22 18:08:52 +0530

    Add files via upload

---
+++ Functions and Arrays/variableLengthArguments.js
@@ -0,0 +1,20 @@
+function sum1(a, b) {
+    return a+b;
+}
+
+var result1 = sum1(4, 5);
+console.log(result1);
+
+function sum2(a, b) {
+    return a+b;
+}
+
+var result2 = sum2(4);
+console.log(result2);
+
+function sum3(a, b) {
+    return a+b;
+}
+
+var result3 = sum3(4, 5, 6);
+console.log(result3);
