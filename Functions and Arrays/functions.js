0ab7f7d1bb32b9f635ed1193595e83eb8b1f2976
built-different-13 <built.different7813@gmail.com>
2025-07-22 11:24:51 +0530

    Add files via upload

---
+++ Functions and Arrays/functions.js
@@ -0,0 +1,24 @@
+function showAlert() {
+    console.log("Shriram Rai");
+}
+
+showAlert();
+
+function showAlert1(msg) {
+    console.log(msg);
+}
+
+showAlert1("Dhara Periwal");
+
+function sum(a, b) {
+    console.log(a+b);
+}
+
+sum(81,31);
+
+function sum1(a, b) {
+    return a+b;
+}
+
+var result = sum1(81, 31);
+console.log(result);
