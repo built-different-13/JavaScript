b123e418284dcd6f7918dce7665de3940b1f9bf9
built-different-13 <built.different7813@gmail.com>
2025-08-04 11:51:55 +0530

    Add files via upload

---
+++ Functions and Arrays/functionsOnArrays.js
@@ -0,0 +1,16 @@
+var arr1 = ["Dhara", 10, true, "Shriram", 20];
+console.log(arr1);
+
+var arr2 = new Array(1000);
+console.log(arr2);
+
+var arr3 = [4,5,6,7];
+console.log(arr3.length);
+arr3.push(10);
+console.log(arr3);
+arr3.pop();
+console.log(arr3);
+arr3.shift();
+console.log(arr3);
+arr3.unshift(8);
+console.log(arr3);
