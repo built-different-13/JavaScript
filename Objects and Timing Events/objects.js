da488d2e905ae81e6971b985be8915db44d6802c
built-different-13 <built.different7813@gmail.com>
2025-08-25 11:17:43 +0530

    Add files via upload

---
+++ Objects and Timing Events/objects.js
@@ -0,0 +1,28 @@
+// Example - 1
+var student = {};
+console.log(student);
+console.log(typeof(student));
+
+// Example - 2
+var student = {
+    name: "Shriram Rai",
+    age: 23,
+    wife: "Dhara Periwal"
+};
+
+console.log(student);
+
+// Example - 3
+var student = {
+    name: "Shriram Rai",
+    age: 23,
+    wife: "Dhara Periwal"
+};
+
+console.log(student);
+console.log(student.age);
+student.age = 22;
+console.log(student);
+console.log(student.sem);
+student.sem=8;
+console.log(student);
