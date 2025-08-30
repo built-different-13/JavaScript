0e3b088380729442f4850b50dae203cbe01c9d79
built-different-13 <built.different7813@gmail.com>
2025-08-30 22:25:30 +0530

    Add files via upload

---
+++ Objects and Timing Events/deleting.js
@@ -0,0 +1,13 @@
+var employee = {
+    name: "Shriram Rai",
+    age: 23,
+    wife: "Dhara Periwal",
+    occupation: "Startup CEO",
+    valuation: "5 Billion USD",
+    income: "700 million USD",
+    hobby: "Painting"
+};
+
+console.log(employee);
+console.log(delete employee.hobby);
+console.log(employee);
