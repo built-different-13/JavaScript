6da3a78a6176e2f34fd69978beb752c9e1fc0990
shriram7813 <built.different7813@gmail.com>
2025-11-10 10:57:26 +0530

    Add files via upload

---
+++ Mini Projects Using JavaScript/const.js
@@ -0,0 +1,23 @@
+// Concept - 1
+var a = 10;
+a = 20;
+console.log(a);
+
+const b = 30;
+console.log(b);
+
+
+// Concept - 2
+var c;
+c = 40;
+console.log(c);
+
+// const d;
+// d = 50;
+
+// Concept - 3
+console.log(e);
+var e = 60;
+
+console.log(f);
+const f = 70;
